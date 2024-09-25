3fcbab2747e1c189eb215d9c66d3de253312df77
rociorial <147448807+rociorial@users.noreply.github.com>
2024-09-25 12:59:07 +0200

    Add files via upload

---
+++ Unidad 1/E0108.java
@@ -0,0 +1,16 @@
+/* Crea un programa que solicite al usuario su edad y muestre si es o no mayor de edad imprimiendo un literal booleano: true o false. */
+
+
+package operadores;
+import java.util.Scanner;
+
+public class E0108 {
+    public static boolean main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce tu edad: ");
+        int edad = scanner.nextInt();
+
+        
+    }
+}
