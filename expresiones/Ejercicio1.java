b6e33e1bea66a065182b1b070abbed93eec50a42
rociorial <rrialgarrido@gmail.com>
2024-10-20 03:45:55 +0200

    borrar

--- expresiones/Ejercicio1.java
+++
@@ -1,9 +0,0 @@
-package ejercicios.expresiones;
-
-public class Ejercicio1 {
-    public static void main(String[] args) {
-
-        int resultado = 5 + 3 * 2;
-        System.out.println("Resultado: " + resultado);
-    }
-}
