d3535efddef0c9fbd84e67dbcb984ef15e12527f
rociorial <rrialgarrido@gmail.com>
2024-10-20 03:10:15 +0200

    ñam

--- operadores/E0108.java
+++ operadores/E0108.java
@@ -12,11 +12,7 @@ public class E0108 {
         System.out.print("Introduce tu edad: ");
         int edad = scanner.nextInt();
         scanner.close();
-<<<<<<< HEAD
-
-=======
         
->>>>>>> 159dfbdb99a25aae6628a573174e08e26a9e37b9
         System.out.println("Eres mayor de edad");
         boolean mayoriaedad = edad >= 18;
         System.out.println(mayoriaedad);
