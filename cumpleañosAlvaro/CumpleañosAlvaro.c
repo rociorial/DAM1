b8a42b8a62201d039a68b61d7318c3c99a78a134
rociorial <rrialgarrido@gmail.com>
2024-11-15 09:45:36 +0100

    Supermercado

--- cumpleañosAlvaro/CumpleañosAlvaro.c
+++ cumpleañosAlvaro/CumpleañosAlvaro.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main{
+int main(){
     for (int i = 0; i < 19; i++){
         printf("Feliz cumpleaños, Álvaro");
         printf("Disfruta de tu %d cumpleaños!", i);
     }
+    return 0;
 }
--- ud2/practicas/Supermercado.java
+++ ud2/practicas/Supermercado.java
@@ -18,7 +18,7 @@ tarjeta” o “en efectivo”.
 ese cliente.
 
     6. Si el método de pago elegido es “en efectivo”, entonces se solicitará el importe
-pagado por el cliente y se calculará el cambio a devover, desglosando dicha
+pagado por el cliente y se calculará el cambio a devolver, desglosando dicha
 cantidad en billetes y monedas de € y de 1 céntimos de euro.
 
     7. Una vez finalizada la compra de un cliente el programa solicitará si se desea repetir
@@ -28,17 +28,91 @@ Implementa el programa utilizando programación modular para descomponer el prob
 en subproblemas más sencillos implementados con funciones. */
 
 package ud2.practicas;
+import java.util.Scanner;
 
 public class Supermercado {
 
-    public static int calcularImporte(double precio, int cantidad) {
-        for(int i = 0; i < cantidad; i++){
-            
-        }
+    public static double calcularImporteTotal (double precio, double precioTotal, int cantidadProductos) {
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.print("Introduzca el precio del producto: ");
+            precio = scanner.nextDouble();
+
+            precioTotal =+ precio;
+
+            if(precio > 0){
+                cantidadProductos++;
+            }
+
+        } while (precio >= 0);
+
+        scanner.close();
+
+        return precioTotal;
+    }
+    
+    public static int metodoPago (int eleccion) {
+        Scanner scanner = new Scanner(System.in);
+        
+        do{
+            System.out.println("Elige el método de pago: ");
+            System.out.println("Efectivo (1)");
+            System.out.println("Tarjeta (2)");
+            eleccion = scanner.nextInt();
+
+            if(eleccion != 1 || eleccion != 2){
+                System.out.println("No has introducido un método correcto");
+            }
+
+        }while (eleccion != 1 || eleccion != 2);
+
+        scanner.close();
+
+        return eleccion; 
     }
 
+    public static void pagoTarjeta (int eleccion) {
+        System.out.println("Pago realizado correctamente.");
+    }
+
+    public static void pagoEfectivo (double efectivo, double precioTotal, 
+    int billetes50, int billetes20, int billetes10, int billetes5, int billetes2, int billetes1,
+    int monedas50, int monedas20, int monedas10, int monedas5, int monedas2, int monedas1) {
+
+        billetes50 = (int)efectivo / 50;
+        efectivo %= 50; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 50
+        billetes20 = (int)efectivo / 20;
+        efectivo %= 20; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 20
+        billetes10 = (int)efectivo / 10;
+        efectivo %= 10; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 10
+        billetes5 = (int)efectivo / 5;
+        efectivo %= 5; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 5
+        billetes2 = (int)efectivo / 2;
+        efectivo %= 2; // Actualiza efectivo para calcular cuánto dinero queda después de sacar las monedas de 2
+        billetes1 = (int)efectivo / 1;
+        efectivo %= 1; // Actualiza efectivo para calcular cuánto dinero queda después de sacar las monedas de 1
+
+        precioTotal = precioTotal * 100; // Pasar a céntimos
+
+        monedas50 = (int)efectivo / 50;
+        efectivo %= 50; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 50
+        monedas20 = (int)efectivo / 20;
+        efectivo %= 20; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 20
+        monedas10 = (int)efectivo / 10;
+        efectivo %= 10; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 10
+        monedas5 = (int)efectivo / 5;
+        efectivo %= 5; // Actualiza efectivo para calcular cuánto dinero queda después de sacar los billetes de 5
+        monedas2 = (int)efectivo / 2;
+        efectivo %= 2; // Actualiza efectivo para calcular cuánto dinero queda después de sacar las monedas de 2
+        monedas1 = (int)efectivo / 1;
+    }
 
     public static void main(String[] args) {
-        
+        int eleccion = 0;
+         
+        calcularImporteTotal(0, 0, 0);
+        metodoPago(eleccion);
+
     }
 }
